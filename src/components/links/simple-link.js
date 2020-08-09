d5497f501238332ef4994447313193d59a2a8837
Teodor Tenchev <teodortenchev@gmail.com>
2020-08-09 22:30:35 +0400

    added form body wrapper and finished signup page

---
+++ src/components/links/simple-link.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './simple-link.module.css';
+
+const SimpleLink = ({ title, to }) => {
+    return (
+        <Link to={to} className={styles.link}>
+            {title}
+        </Link>
+    )
+}
+
+export default SimpleLink;
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,72 +1,72 @@
-import React, { Component } from 'react';
-import styles from './index.module.css';
-import PageLayout from '../../components/page-layout';
+import React, { useState } from 'react';
+import SimpleLink from '../../components/links/simple-link';
+import FormControl from '../../components/form-control'
 import Title from '../../components/title'
-//import SubmitButton from '../../components/button/submit-button';
-import FormControl from '../../components/formControl';
-//import authenticate from '../../utils/authenticate';
-import UserContext from '../../Context';
+import PageLayout from '../../components/page-layout';
+import Button from '../../components/button/submit-button';
+import FormWrapper from '../../components/form-wrapper';
 
-class RegisterPage extends Component {
 
-    constructor(props) {
-        super(props);
+const RegisterPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [rePassword, setRePassword] = useState('');
 
-        this.state = {
-            username: "",
-            password: "",
-            rePassword: ""
-        };
-    }
+    const [displayName, setDisplayName] = useState('');
 
-   // static contextType = UserContext
+    const [error, setError] = useState(null);
 
-    handleSubmit = async event => {
+    const createUserWithEmailAndPasswordHandler = (event, email, password, rePassword) => {
         event.preventDefault();
+        setEmail('');
+        setPassword('');
+        setDisplayName('');
+        setRePassword('')
+    };
 
-        const { username, password, rePassword } = this.state;
 
-        await authenticate('http://localhost:9999/api/user/register', { username, password }, (user) => {
-            console.log('Successful register');
-            this.context.logIn(user);
-            this.props.history.push('/');
-        }, (e) => {
-            console.log('Error', e)
-        })
+    const onChangeHandler = (event) => {
+        const { name, value } = event.currentTarget;
+
+        if (name === 'userEmail') {
+            setEmail(value);
+        }
+        else if (name === 'userPassword') {
+            setPassword(value);
+        }
+        else if (name === 'displayName') {
+            setDisplayName(value)
+        }
     }
 
-    handleChange = (event, type) => {
-        const newState = {};
-        newState[type] = event.target.value;
-        this.setState(newState);
-    }
+    return (
+        <PageLayout>
 
+            <FormWrapper>
+                <Title title="Sign Up" />
+                <FormControl htmlFor="displayName" fieldName="DisplayName" type="text" name="displayName"
+                    value={displayName} placeholder="Your Name (e.g. John Doe)" id="displayName"
+                    onChange={(event) => onChangeHandler(event)} />
+                <FormControl htmlFor="userEmail" fieldName="Email" type="email" name="userEmail"
+                    value={email} placeholder="Email Address (e.g. mishka@gmail.com)" id="userEmail"
+                    onChange={(event) => onChangeHandler(event)} />
 
-    render() {
+                <FormControl htmlFor="userPassword" fieldName="Password" type="password" name="userPassword"
+                    value={password} placeholder="Password" id="userPassword"
+                    onChange={(event) => onChangeHandler(event)} />
 
-        const { username, password, rePassword } = this.state;
+                <FormControl htmlFor="rePassword" fieldName="Confirm Password" type="password" name="rePassword"
+                    value={rePassword} placeholder="Confirm password" id="rePassword"
+                    onChange={(event) => onChangeHandler(event)} />
 
-        return (
-            <PageLayout>
-                <Title title="Register Page" />
+                <Button title="Register"
+                    onClick={(event) => createUserWithEmailAndPasswordHandler(event, email, password)} />
 
-                <div className={styles.container}>
+                <SimpleLink title="Already registered? Log in here" to="/login" />
 
-                    <form onSubmit={this.handleSubmit} >
-                        <FormControl fieldName="Email" fieldType="text" id="username"
-                            autocomplete="" value={username} onChange={(e) => this.handleChange(e, 'username')} />
-                        <FormControl fieldName="Password" fieldType="password" id="password"
-                            autocomplete="" value={password} onChange={(e) => this.handleChange(e, 'password')} />
-                        <FormControl fieldName="Confirm Password" fieldType="password" id="rePassword"
-                            autocomplete="" value={rePassword} onChange={(e) => this.handleChange(e, 'rePassword')} />
-                        <div>
-                            <SubmitButton title="Register" />
-                        </div>
-                    </form>
-                </div>
-            </PageLayout>
-        )
-    }
+            </FormWrapper>
+        </PageLayout>
+    )
 }
 
 export default RegisterPage;
