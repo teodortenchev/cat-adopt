f16345fd2144a6c7dcc885ba32670564fedf57c2
Teodor Tenchev <teodortenchev@gmail.com>
2020-08-09 16:26:16 +0400

    finished styling the log in page

--- src/components/button/submit-button.js
+++ src/components/button/submit-button.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './submit-button.module.css';
+import Button from '@material-ui/core/Button';
 
-const SubmitButton = ({ title }) => {
+
+const SubmitButton = ({ title, onClick }) => {
     return (
-        <button type="submit" className={styles.submit}>{title}</button>
+        <div className={styles.buttonContainer}>
+            <Button variant="contained" color="default" size="small" className={styles.button} onClick={onClick}>{title}</Button>
+        </div>
     )
 }
 
