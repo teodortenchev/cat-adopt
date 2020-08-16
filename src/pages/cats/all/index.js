536a3c2b859a61bee16c8130cb21b554a0c4839a
Teodor Tenchev <teodortenchev@gmail.com>
2020-08-17 01:01:52 +0400

    added cat cards and views

--- src/pages/cats/all/index.js
+++ src/pages/cats/all/index.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Title from '../../../components/title'
-import PageLayout from "../../layouts/page-layout-nobg";
+import PageLayout from '../../layouts/page-layout';
+import ContentWrapper from '../../../components/content-wrapper'
 
 import firebase from '../../../utils/firebase';
+import CatCard from '../../../components/cat';
+import styles from './index.module.css';
 
 const AllCatsPage = (props) => {
 
     const [cats, setCats] = useState([]);
-    const [roles, setRoles] = useState([]);
-
 
     useEffect(() => {
         const fetchData = async () => {
@@ -22,11 +23,15 @@ const AllCatsPage = (props) => {
 
     return (
         <PageLayout>
-            <ul>
+            <ContentWrapper>
+
                 {cats.map(cat => (
-                    <li key={cat.id}><h1>{cat.name}</h1></li>
+                    <div className={styles.cat}>
+                    <CatCard name={cat.name} breed={cat.breed} story={cat.story} id={cat.id} />
+                    </div>
                 ))}
-            </ul>
+
+            </ContentWrapper>
         </PageLayout>
     )
 }
--- src/pages/home-guest/index.js
+++ src/pages/home-guest/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import PageLayout from "../layouts/page-layout";
 import styles from './index.module.css';
 import Title from '../../components/title';
+import SimpleLink from '../../components/link'
 
 const HomePageGuest = () => {
     return (
         <PageLayout>
             <div className={styles.container}>
-                <Title title= "Looking to adopt a cat?" />
-                <p>The Cat Adopt Foundation shelter has countless deserving cats in need of loving homes. 
-                    Register and search our website to find the perfect companion for your family. 
-                    Our cats can easily travel to Europe, the US and Canada. The process is very straightforward 
+                <Title title="Looking to adopt a cat?" />
+                <div className={styles.text}>
+                    <p>The Cat Adopt Foundation shelter has countless deserving cats in need of loving homes.
+                    Register and search our website to find the perfect companion for your family.
+                    Our cats can easily travel to Europe, the US and Canada. The process is very straightforward
                     and we will help you every step of the way.</p>
+                    <p>You can always <SimpleLink title="browse the cats" styleType="main-page" /> that are up for adoption, but if you want to begin the process
+                        of adopting a kitty you must <SimpleLink title="sign up for an account" styleType="main-page" to="/register" />.
+                    </p>
+                </div>
             </div>
         </PageLayout>
     )
