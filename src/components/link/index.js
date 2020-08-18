f7abcba2a5307c78e01e66615d6fee46ded277a9
Teodor Tenchev <teodortenchev@gmail.com>
2020-08-18 23:31:15 +0400

    Update index.js

--- src/components/link/index.js
+++ src/components/link/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './index.module.css';
 
-const CustomLink = ({ title, to, styleType, icon }) => {
+const CustomLink = ({ title, to, styleType }) => {
     return (
         <Link to={to} className={styles[styleType]}>
-            {icon}{title}
+            {title}
         </Link>
     )
 }
