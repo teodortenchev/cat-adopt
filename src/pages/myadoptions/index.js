3866a814c12ad442eb67dfa99c0d57a6078e5b7b
Teodor Tenchev <teodortenchev@gmail.com>
2020-08-21 01:25:25 +0400

    implemented the my adoptions page

---
+++ src/pages/myadoptions/index.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect, useContext } from 'react';
+import Title from '../../components/title'
+import PageLayout from '../layouts/page-layout'
+import ContentWrapper from '../../components/content-wrapper';
+import firebase from '../../utils/firebase';
+import styles from './index.module.css';
+import { Cube } from 'react-preloaders';
+import AdoptionRow from '../../components/adoptionrow';
+import UserContext from '../../Context';
+
+const MyAdoptionsPage = (props) => {
+
+    const [cats, setCats] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { appUser } = useContext(UserContext);
+    const id = appUser.uid;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const db = firebase.db;
+            const data = await db.collection("cats").where("pendingAdoption", "==", true).where("requestedBy", "==", id).get().then(setLoading(true));
+            setCats(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+        };
+        fetchData().then(setLoading(false));
+    }, [id]);
+
+    console.log(cats);
+    console.log(id);
+
+    return (
+        <PageLayout>
+            <ContentWrapper>
+                <Cube customLoading={loading} />
+
+                <div className={styles.container}>
+                    <Title title="Your Adoption Requests" />
+                    {cats.map(cat => (
+                        <div className={styles.cat} key={cat.id}>
+                            <AdoptionRow catName={cat.name} catPhoto={cat.imageUrl} />
+                        </div>
+                    ))}
+
+                </div>
+
+            </ContentWrapper>
+        </PageLayout>
+    )
+}
+
+export default MyAdoptionsPage;
